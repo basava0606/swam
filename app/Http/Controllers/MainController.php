bbe9bae3b2c3f5d4f796dd1c3fd8319d8c41fbde
nagasreeu <nnnagasree@gmail.com>
2019-04-24 13:31:04 +0530

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CRUDBooster;
+use Session;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class MainController extends Controller
+{
+
+}
